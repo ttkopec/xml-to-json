645f6d640782eb9347f2368bdacf26f3f673f021
ttkopec <tomek.kopec1@gmail.com>
2018-05-11 15:56:42 +0200

    Refactor code

--- xmltojson/__init__.py
+++ xmltojson/__init__.py
@@ -1,48 +1,54 @@
 import os
 
 import lxml.etree as ET
-import xmltojson._pyang as _pyang
+import pyang.stub as stub
+
+_dir = os.path.dirname(os.path.realpath(__file__))
+xsl_path = os.path.join(_dir, 'out.xsl')
+xml_path = os.path.join(_dir, 'out.xml')
+_files = [xsl_path, xml_path]
 
 
-class PyangWrapper:
-    _dir = os.path.dirname(os.path.realpath(__file__))
-    xsl = os.path.join(_dir, 'out.xsl')
-    xml = os.path.join(_dir, 'out.xml')
-    _files = [xsl, xml]
+def gen_xslt_string(models, yang_directory):
+    stub.run(models, 'jsonxsl', xsl_path, yang_directory)
 
-    @staticmethod
-    def gen_xslt_string(models, yang_directory):
-        _pyang.run(models, 'jsonxsl', PyangWrapper.xsl, yang_directory)
 
-    @staticmethod
-    def gen_sample_xml_string(models, yang_directory):
-        _pyang.run(models, 'sample-xml-skeleton', PyangWrapper.xml, yang_directory, sample_defaults=True)
+def gen_sample_xml_string(models, yang_directory):
+    stub.run(models, 'sample-xml-skeleton', xml_path, yang_directory, sample_defaults=True)
 
-    @staticmethod
-    def cleanup():
-        for f in PyangWrapper._files:
-            try:
-                os.remove(f)
-            except FileNotFoundError:
-                pass
 
-    @staticmethod
-    def parse(xsl, xml):
-        xsl_tree = ET.parse(xsl)
-        xml_tree = ET.parse(xml)
-
-        transform = ET.XSLT(xsl_tree)
-        transformed_xml = transform(xml_tree)
-
-        return transform.tostring(transformed_xml)
-
-    @staticmethod
-    def yangs_to_json(models, yang_directory):
+def cleanup():
+    for f in _files:
         try:
-            PyangWrapper.gen_sample_xml_string(models, yang_directory)
-            PyangWrapper.gen_xslt_string(models, yang_directory)
+            os.remove(f)
+        except FileNotFoundError:
+            pass
 
-            return PyangWrapper.parse(*PyangWrapper._files)
-        except Exception as e:
-            PyangWrapper.cleanup()
-            raise e
+
+def parse(xsl, xml):
+    xsl_tree = ET.parse(xsl)
+    xml_tree = ET.parse(xml)
+
+    transform = ET.XSLT(xsl_tree)
+    transformed_xml = transform(xml_tree)
+
+    return transform.tostring(transformed_xml)
+
+
+def yangs_to_json(models, yang_directory, do_cleanup=True):
+    try:
+        cleanup()
+
+        gen_sample_xml_string(models, yang_directory)
+        gen_xslt_string(models, yang_directory)
+
+        result = parse(*_files)
+
+        if do_cleanup:
+            cleanup()
+
+        return result
+    except Exception as e:
+        if do_cleanup:
+            cleanup()
+        raise e
